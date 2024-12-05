ec94300d39513c30a9e82924c18f66cc2313d74a
rahul2011421 <rahulyadavv2011421@gmail.com>
2024-12-05 20:38:21 +0530

    first commit

---
+++ src/main/java/com/rahul/blog/controllers/UserController.java
@@ -0,0 +1,10 @@
+package com.rahul.blog.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+}
